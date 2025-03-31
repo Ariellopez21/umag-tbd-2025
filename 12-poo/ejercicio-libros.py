13a2194ab83a2c9a88b8776da504b4d7e70d9491
Ariellopez21 <arilopez@umag.cl>
2025-03-31 09:40:53 -0300

    POO - Tarea Finalizada

--- 12-poo/ejercicio-libros.py
+++ 12-poo/ejercicio-libros.py
@@ -15,7 +15,7 @@ class Libro:
         self.disponible = True
 
 class Usuario:
-    def __init__(self, nombre: str, id: int, prestamos: list[str]) -> None:
+    def __init__(self, nombre: str, id: int, prestamos: list["Libro"]) -> None:
         self.nombre = nombre
         self.id = id
         self.prestamos = prestamos
@@ -23,30 +23,84 @@ class Usuario:
     def tomar_libro(self, libro: "Libro") -> None:
         if libro.disponible:
             libro.prestar()
-            self.prestamos.append(libro.titulo)
+            self.prestamos.append(libro)
         else:
             print(f"El libro {libro.titulo} no está disponible.")
 
     def devolver_libro(self, libro: "Libro") -> None:
         if libro.titulo in self.prestamos:
             libro.devolver()
-            self.prestamos.remove(libro.titulo)
+            self.prestamos.remove(libro)
         else:
             print(f"No tienes el libro {libro.titulo} prestado.")
 
 class Biblioteca:
-    def __init__(self, nombre: str, libros: list[Libro], usuarios: list[Usuario]) -> None:
+    def __init__(self, nombre: str, libros: list["Libro"], usuarios: list["Usuario"]) -> None:
         self.nombre = nombre
         self.libros = libros
         self.usuarios = usuarios
 
-print("QUIJOTE:")
-quijote = Libro("Don Quijote de la Mancha", "Miguel de Cervantes", "978-3-16-148410-0", True)
-print(quijote.mostrar_info())
+    def agregar_libro(self, libro: "Libro") -> None:
+        self.libros.append(libro)
 
-print("USUARIO:")
-ariel = Usuario("Ariel", 1, [quijote.titulo])
-print("Prestamos:", ariel.prestamos)
-ariel.devolver_libro(quijote)
-print("Prestamos:", ariel.prestamos)
+    def registrar_usuario(self, usuario: "Usuario") -> None:
+        self.usuarios.append(usuario)
 
+    def mostrar_libros_disponibles(self) -> None:
+        for libro in self.libros:
+            if libro.disponible:
+                print(libro.mostrar_info())
+            else:
+                print(libro.titulo, " -> no disponible")
+
+    def mostrar_usuarios(self) -> None:
+        if self.usuarios:
+            for usuario in self.usuarios:
+                print(usuario.nombre)
+
+    def buscar_libro_por_titulo(self, titulo : str) -> bool:
+        for libro in self.libros:
+            if libro.titulo == titulo and libro.disponible:
+                return True
+        print(titulo, " -> no disponible")
+        return False
+
+quijote = Libro("Don Quijote de la Mancha", "Mike de Cervantes", "123-4", True)
+pythonLearning = Libro(titulo="Learning Python3, 6ta edición", autor="Diego Alvarez S.", isbn="002231-3", disponible=True)
+ariel = Usuario("Ariel", 1, [])
+mike = Usuario(nombre="Mike L. Loaiza", id=2, prestamos=[])
+biblioteca_umag = Biblioteca("Biblioteca de la Umag", [quijote], [ariel])
+
+print("Biblioteca UMAG, libros disponibles:")
+biblioteca_umag.mostrar_libros_disponibles()
+
+print("Biblioteca UMAG, usuarios:")
+biblioteca_umag.mostrar_usuarios()
+
+biblioteca_umag.agregar_libro(pythonLearning)
+biblioteca_umag.registrar_usuario(mike)
+
+print()
+print("BUSCAR LIBRO", quijote.titulo ,":")
+print(biblioteca_umag.buscar_libro_por_titulo(quijote.titulo))
+
+print()
+
+print("REFRESH: Biblioteca UMAG, libros disponibles:")
+biblioteca_umag.mostrar_libros_disponibles()
+
+print("REFRESH: Biblioteca UMAG, usuarios:")
+biblioteca_umag.mostrar_usuarios()
+
+ariel.tomar_libro(quijote)
+mike.tomar_libro(pythonLearning)
+
+print()
+
+print("REFRESH 2: Biblioteca UMAG, libros disponibles:")
+biblioteca_umag.mostrar_libros_disponibles()
+
+print()
+
+print("BUSCAR LIBRO 2 ", pythonLearning.titulo ,":")
+print(biblioteca_umag.buscar_libro_por_titulo(pythonLearning.titulo))
